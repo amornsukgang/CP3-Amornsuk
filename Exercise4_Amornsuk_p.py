f58a7b313b2984378401676fb2b187de990198df
amornsukgang <85466643+amornsukgang@users.noreply.github.com>
2025-06-24 11:26:01 +0700

    Create Exercise4_Amornsuk_p.py

---
+++ Exercise4_Amornsuk_p.py
@@ -0,0 +1,16 @@
+
+Foundation_English=float(input("Foundation English : "))
+General_Business =float(input("General Business : "))
+Introduction_to_Computer_Systems = float(input("Introduction to Computer Systems : "))
+Computer_Programming =float(input("Computer Programming  : "))
+
+
+
+
+
+
+print("--- Your Score ---")
+print("Foundation English : คะแนนที่ได้ =",Foundation_English)
+print("General Business : คะแนนที่ได้ =",General_Business)
+print("Introduction to Computer Systems : คะแนนที่ได้ =",Introduction_to_Computer_Systems)
+print("Computer Programming : คะแนนที่ได้ =",Computer_Programming)
