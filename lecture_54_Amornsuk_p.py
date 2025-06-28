84c93b31936320818df3e766c84527dff770003c
amornsukgang <85466643+amornsukgang@users.noreply.github.com>
2025-06-28 22:06:12 +0700

    Create lecture_54_Amornsuk_p.py

---
+++ lecture_54_Amornsuk_p.py
@@ -0,0 +1,34 @@
+
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return showMenu()
+    else:
+        return print("Wrong username or password")
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+    return menuSelect()
+
+def menuSelect():
+    userSelected = int(input(">>"))
+    if userSelected == 1:
+        return vatCalculator(totalPrice = int(input("Cost of goods: ")))
+    else:
+        return priceCalculator()
+
+
+def vatCalculator(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return print(result,"THB")
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculator(price1 + price2)
+
+login()
