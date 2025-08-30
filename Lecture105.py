fbbfff5db25336469c1a58c1543eab972d2aa895
amornsukgang <85466643+amornsukgang@users.noreply.github.com>
2025-08-30 20:47:55 +0700

    Create Lecture105.py

---
+++ Lecture105.py
@@ -0,0 +1,25 @@
+class Vehicle:
+    lecenseCode = ""
+    serialCode = ""
+    def turnOnAirConditioner(self):
+        print("Turn On : Air")
+
+class Pickup(Vehicle):
+    pass;
+
+class Car(Vehicle):
+    pass;
+
+class Van(Vehicle):
+    pass;
+
+car1 = Car()
+car1.turnOnAirConditioner()
+
+pickup1 = Pickup()
+pickup1.turnOnAirConditioner()
+
+van1 = Van()
+van1.turnOnAirConditioner()
+
+
